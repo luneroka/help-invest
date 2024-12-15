9acc17728e9e3bab4ff18c70a5df0f88e36606ca
Yoann Robert <luneroka@gmail.com>
2024-12-15 10:40:56 +0100

    Modified Portfolios table and added Transactions table

--- models.py
+++ models.py
@@ -15,12 +15,19 @@ class Categories(db.Model):
 
 
 class Portfolios(db.Model):
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+  category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), primary_key=True)
+  balance = db.Column(db.Integer, nullable=False, default=0)
+
+class Transactions(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
   category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
   amount = db.Column(db.Integer, nullable=False)
   timestamp = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
 
-  # Define relationships for easier querying
-  user = db.relationship('Users', backref='portfolios')
-  category = db.relationship('Categories', backref='portfolios')
+
+# Define relationships for easier querying
+user = db.relationship('Users', backref='portfolios')
+category = db.relationship('Categories', backref='portfolios')
+
