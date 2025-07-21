1ebf5f1052d436f62b694bfb4b6d511c3b9250d1
Luneroka <luneroka@gmail.com>
2025-07-21 13:18:09 +0000

    Unit test on RiskCard

--- frontend/src/components/elements/RiskCard.jsx
+++ frontend/src/components/elements/RiskCard.jsx
@@ -100,7 +100,9 @@ function RiskCard() {
           )}
 
           <div className='flex flex-col gap-4'>
-            <p className='text-center'>Modifiez votre profil de risque :</p>
+            <label htmlFor='risk-profile' className='text-center'>
+              Modifiez votre profil de risque :
+            </label>
             <select
               name='riskProfile'
               id='risk-profile'
